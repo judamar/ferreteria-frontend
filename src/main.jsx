2ddfa03e9bc642534cde8cc6246cfa9ca45e54a6
judamar <juancho.o.mart@gmail.com>
2025-11-04 17:42:49 -0500

    remove bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css'
 import '@fortawesome/fontawesome-free/css/all.min.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle.js'
 import axios from 'axios'
 
 window.axios = axios
