8df25073048f953c21075d99d6a24c722308786b
judamar <juancho.o.mart@gmail.com>
2025-11-04 17:42:07 -0500

    env variables manager

---
+++ src/config/env.js
@@ -0,0 +1,13 @@
+export const ENV = {
+    BACKEND_URL: import.meta.env.VITE_BACKEND_URL,
+    PHONE_NUM: import.meta.env.VITE_PHONE_NUM,
+    NAME: import.meta.env.VITE_NAME,
+    LOGO: import.meta.env.VITE_LOGO,
+    NIT: import.meta.env.VITE_NIT,
+    DIR: import.meta.env.VITE_DIR,
+    CIUDAD: import.meta.env.VITE_CIUDAD,
+    TEL: import.meta.env.VITE_TEL,
+    EMAIL: import.meta.env.VITE_EMAIL
+}
+
+
