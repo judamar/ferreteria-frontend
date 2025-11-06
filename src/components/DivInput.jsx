7e97243906f20538ec3178ca3a9df830d4061ed0
judamar <juancho.o.mart@gmail.com>
2025-11-06 10:31:20 -0500

    improve DivInput.jsx

--- src/components/DivInput.jsx
+++ src/components/DivInput.jsx
@@ -1,20 +1,43 @@
 import React, {forwardRef, useEffect, useRef} from 'react'
 
-// eslint-disable-next-line react-refresh/only-export-components, react/display-name, react/prop-types
-export default forwardRef(({ type='text', icon='user', placeholder='', name, id, value, className, required, isFocused, handleChange}, ref) => {
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  const input = ref ? ref: useRef()
-  useEffect(()=>{
-    if(isFocused){
+export default forwardRef(({
+                             label,
+                             type = 'text',
+                             icon = 'user',
+                             placeholder = '',
+                             name,
+                             id,
+                             value,
+                             className,
+                             required,
+                             isFocused,
+                             handleChange
+                           }, ref) => {
+  const input = ref ? ref : useRef()
+  useEffect(() => {
+    if (isFocused) {
       input.current.focus()
     }
-  },[input, isFocused])
+  }, [input, isFocused])
   return (
-    <div className='input-group mb-3'>
-      <span className='input-group-text'>
-        <i className={`fa-solid ${icon}`}/>
-      </span>
-      <input type={type} placeholder={placeholder} name={name} id={id} value={value || ''} className={className} href={input} required={required} onChange={(e) => handleChange(e)}/>
+
+    <div>
+      <label htmlFor={id} className="block text-lg font-medium text-gray-900">
+        {label}
+      </label>
+      <div
+        className='mt-2 flex justify-items-center items-center bg-white rounded-md px-3 py-1.5 outline-1 -outline-offset-1 outline-gray-600 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-gray-600 gap-2'>
+        <i className={`${icon} text-gray-900 text-2xl`}/>
+        <input type={type}
+               placeholder={placeholder}
+               name={name}
+               id={id}
+               value={value || ''}
+               className={className || "block w-full bg-transparent text-black placeholder-gray-600 focus:outline-none text-lg"}
+               ref={input}
+               required={required}
+               onChange={(e) => handleChange(e)}/>
+      </div>
     </div>
   )
 })
