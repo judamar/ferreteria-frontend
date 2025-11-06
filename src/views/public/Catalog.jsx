0bd2267a8d21444e6802642463ed527efc903ce5
judamar <juancho.o.mart@gmail.com>
2025-11-05 19:01:50 -0500

    removed select of categories (temporary)

--- src/views/public/Catalog.jsx
+++ src/views/public/Catalog.jsx
@@ -63,6 +63,7 @@ const Catalog = () => {
             getProducts(e.target.value)
           }}
         />
+        */}
       </div>
       <div
         className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6 mt-10 flex justify-items-center">
