0ff3f2a20122e098ce47d5f23d69fcbd052f98a2
judamar <juancho.o.mart@gmail.com>
2025-11-06 17:05:53 -0500

    update color

--- src/components/DivInput.jsx
+++ src/components/DivInput.jsx
@@ -26,14 +26,14 @@ export default forwardRef(({
         {label}
       </label>
       <div
-        className='mt-2 flex justify-items-center items-center bg-white rounded-md px-3 py-1.5 outline-1 -outline-offset-1 outline-gray-600 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-gray-600 gap-2'>
+        className='mt-2 flex justify-items-center items-center bg-white rounded-md px-3 py-1.5 outline-1 -outline-offset-1 outline-gray-700 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-gray-700 gap-2'>
         <i className={`${icon} text-gray-900 text-2xl`}/>
         <input type={type}
                placeholder={placeholder}
                name={name}
                id={id}
                value={value || ''}
-               className={className || "block w-full bg-transparent text-black placeholder-gray-600 focus:outline-none text-lg"}
+               className={className || "block w-full bg-transparent text-gray-900 placeholder-gray-900 focus:outline-none text-lg"}
                ref={input}
                required={required}
                onChange={(e) => handleChange(e)}/>
