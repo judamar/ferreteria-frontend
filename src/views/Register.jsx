6e62ef5ac39cf5fa58ab89cb91dea6fb39e6e100
judamar <juancho.o.mart@gmail.com>
2025-11-06 10:32:03 -0500

    Improve Register.jsx form using tailwind

--- src/views/Register.jsx
+++ src/views/Register.jsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react'
-import { useNavigate, Link } from 'react-router-dom'
-import { sendRequest } from '../functions.jsx'
+import {useNavigate, Link} from 'react-router-dom'
+import {sendRequest} from '../functions.jsx'
 import DivInput from '../components/DivInput.jsx'
 
 const API_URL = import.meta.env.API_URL
@@ -23,32 +23,93 @@ const Register = () => {
     }
   }
   return (
-    <div className='container-fluid'>
-      <div className='row mt-5'>
-        <div className='col-md-4 offset-md-4'>
-          <div className='card border border-danger'>
-            <div className="card-header bg-danger border border-danger text-white">
-              Registrarse
-            </div>
-            <div className='card-body'>
-              <form onSubmit={register}>
-                <DivInput type='number' icon='fa-at' value={cedula} className='form-control' placeholder='Cédula' required='required' handleChange={(e)=> setCedula(e.target.value)}/>
-                <DivInput type='text' icon='fa-user' value={nombre_completo} className='form-control' placeholder='Nombre Completo' required='required' handleChange={(e)=> setNombre_completo(e.target.value)}/>
-                <DivInput type='email' icon='fa-envelope' value={correo_electronico} className='form-control' placeholder='Correo Electrónico' handleChange={(e)=> setCorreo_electronico(e.target.value)}/>
-                <DivInput type='number' icon='fa-phone' value={telefono} className='form-control' placeholder='Teléfono' required='required' handleChange={(e)=> setTelefono(e.target.value)}/>
-                <DivInput type='text' icon='fa-location-dot' value={direccion} className='form-control' placeholder='Dirección' handleChange={(e)=> setDireccion(e.target.value)}/>
-                <DivInput type='password' icon='fa-key' value={password} className='form-control' placeholder='Contraseña' required='required' handleChange={(e)=> setPassword(e.target.value)}/>
-                <div className='d-grid col-10 mx-auto'>
-                  {/* rome-ignore lint/a11y/useButtonType: <explanation> */}
-                  <button className='btn btn-danger'>
-                    <i className='fa-solid fa-door-open'/> Registrarse
-                  </button>
-                </div>
-              </form>
-              <Link to='/login'>
-                <i className='fa-solid fa-user'/>Iniciar Sesión
+    <div className='container mx-auto my-auto p-6 flex flex-col items-center justify-center gap-6'>
+      <div className='flex flex-col lg:px-8 w-full max-w-6xl bg-white rounded-2xl shadow-md p-6'>
+        <div className='w-full'>
+          <h3 className="title-h3">
+            Regístrate
+          </h3>
+          <div className='mt-5 w-full'>
+            <form onSubmit={register} className="space-y-6">
+              <div className='grid grid-cols-1 lg:grid-cols-2 gap-6'>
+                <DivInput
+                  label="Cédula"
+                  id="cedula"
+                  type='number'
+                  icon='icon-[material-symbols--id-card-outline]'
+                  value={cedula}
+                  placeholder='Cédula'
+                  required='required'
+                  handleChange={(e) =>
+                    setCedula(e.target.value)}/>
+
+                <DivInput
+                  label="Nombre completo"
+                  id="name"
+                  type='text'
+                  icon='icon-[lets-icons--user]'
+                  value={nombre_completo}
+                  placeholder='Nombre Completo'
+                  required='required'
+                  handleChange={(e) =>
+                    setNombre_completo(e.target.value)}/>
+
+                <DivInput
+                  label="Email"
+                  id="email"
+                  type='email'
+                  icon='icon-[mi--email]'
+                  value={correo_electronico}
+                  placeholder='Correo Electrónico'
+                  handleChange={(e) =>
+                    setCorreo_electronico(e.target.value)}/>
+
+                <DivInput
+                  label="Telefono"
+                  id="phone"
+                  type='number'
+                  icon='icon-[ci--phone]'
+                  value={telefono}
+                  placeholder='Teléfono'
+                  required='required'
+                  handleChange={(e) =>
+                    setTelefono(e.target.value)}/>
+
+                <DivInput
+                  label="Dirección"
+                  id="address"
+                  type="text"
+                  icon="icon-[tdesign--location]"
+                  value={direccion}
+                  placeholder="Dirección"
+                  handleChange={(e) =>
+                    setDireccion(e.target.value)}/>
+
+                <DivInput
+                  label="Contraseña"
+                  id="password"
+                  type="password"
+                  icon="icon-[gg--password]"
+                  value={password}
+                  placeholder='Contraseña'
+                  required='required'
+                  handleChange={(e) =>
+                    setPassword(e.target.value)}/>
+              </div>
+              <div>
+                <button
+                  type="submit"
+                  className='flex w-full items-center justify-center rounded-md bg-red-600 px-3 py-1.5 text-lg font-semibold text-white hover:bg-red-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600 gap-2 transition'>
+                  <i className='fa-solid fa-user-plus'/> Registrarse
+                </button>
+              </div>
+            </form>
+            <p className="mt-3 text-center text-sm text-gray-600">
+              ¿Ya tienes cuenta?
+              <Link to="/login" className="ml-1 font-semibold text-red-600 hover:text-red-500">
+                Inica Sesión
               </Link>
-            </div>
+            </p>
           </div>
         </div>
       </div>
