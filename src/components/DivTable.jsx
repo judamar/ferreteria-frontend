dce237c6ef5c9116edd5d4cb23946a82768afd4e
judamar <juancho.o.mart@gmail.com>
2025-11-06 17:06:11 -0500

    using tailwind

--- src/components/DivTable.jsx
+++ src/components/DivTable.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 
-const DivTable = ({ children, col, off, classLoad, classTable }) => {
+const DivTable = ({children, col, off, classLoad, classTable}) => {
   return (
-    <div className='row mt-3'>
-      <div className={`col-md-${col} offset-md-${off}`}>
-        <div className={`card border border-dark text-center ${classLoad}`}>
-          <div className={`table-responsive ${classTable}`}>
+    <div className='container mx-auto px-4 mt-3'>
+      <div className={`
+    ${col === '12' ? 'w-full' : ''}
+    ${col === '10' ? 'w-10/12 mx-auto' : ''}
+    ${col === '8' ? 'w-8/12 mx-auto' : ''}
+    ${col === '6' ? 'w-6/12 mx-auto' : ''}
+    ${col === '4' ? 'w-4/12 mx-auto' : ''}
+  `}>
+        <div className={`bg-white border-2 border-gray-900 rounded-lg shadow-md text-center ${classLoad}`}>
+          <div className={`overflow-x-auto ${classTable}`}>
             {children}
           </div>
         </div>
