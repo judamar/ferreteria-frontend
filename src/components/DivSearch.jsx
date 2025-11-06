20e42599ba0826a943ef44111ee52bba2d736b9e
judamar <juancho.o.mart@gmail.com>
2025-11-06 09:03:14 -0500

    change color for text and placeholder

--- src/components/DivSearch.jsx
+++ src/components/DivSearch.jsx
@@ -6,7 +6,7 @@ const DivSearch = ({placeholder, handleChange, value, handleSearchSubmit}) => {
       <div className="flex gap-2">
         <div className="flex-1 relative">
           <input
-            className="w-full px-4 py-2 pl-10 border-2 bg-white border-gray-600 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-600 hover:border-gray-700 transition-colors"
+            className="w-full px-4 py-2 pl-10 bg-white border-2 border-gray-600 text-black placeholder-gray-600 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-600 hover:border-gray-700 transition-colors"
             type="search"
             placeholder={placeholder}
             aria-label="Search"
