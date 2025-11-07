a9f73954c7b3c2c7261cb858b2cf7a921653f491
judamar <juancho.o.mart@gmail.com>
2025-11-07 16:34:03 -0500

    improve select

--- src/components/DivSelect.jsx
+++ src/components/DivSelect.jsx
@@ -1,25 +1,58 @@
 import React, {forwardRef, useEffect, useRef} from 'react'
 
-export default forwardRef(({ options=[], sel, sel2='', separator='', icon='user', name, id, value, className, placeholder='Seleccionar', required, disabled='disabled', isFocused, handleChange}, ref) => {
-  const input = ref ? ref: useRef()
-  useEffect(()=>{
-    if(isFocused){
+export default forwardRef(({
+                             options = [],
+                             sel,
+                             sel2 = '',
+                             separator = '',
+                             icon = 'user',
+                             label,
+                             name,
+                             id,
+                             value,
+                             placeholder = 'Seleccionar',
+                             required,
+                             disabled = 'disabled',
+                             isFocused,
+                             handleChange
+                           }, ref) => {
+  const input = ref ? ref : useRef()
+  useEffect(() => {
+    if (isFocused) {
       input.current.focus()
     }
-  },[input, isFocused])
+  }, [input, isFocused])
   return (
-    <div className='input-group mb-3'>
-      <span className='input-group-text'>
-        <i className={`fa-solid ${icon}`}/>
-      </span>
-      <select name={name} id={id} value={value} className={className} href={input} required={required} onChange={(e) => handleChange(e)}>
-        <option value="" disabled={disabled}>{placeholder}</option>
-        { options.map((op) => (
-          <option value={op.id} key={op.id}>
-            {op[sel]}{separator}{op[sel2]}
+    <div>
+      <label htmlFor={id} className="block text-lg font-medium text-gray-900">
+        {label}
+      </label>
+      <div
+        className="mt-2 flex items-center bg-white rounded-md px-3 py-1.5 outline -outline-offset-1 outline-gray-700 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-gray-700 gap-2"
+      >
+        <i className={`${icon} text-gray-900 text-2xl`}/>
+        <select
+          name={name}
+          id={id}
+          value={value}
+          required={required}
+          onChange={handleChange}
+          className="flex-1 bg-transparent outline-none text-gray-900 text-lg py-1 px-2"
+        >
+          <option value="" disabled>
+            {placeholder}
           </option>
-        ))}
-      </select>
+          {options.map((op) => (
+            <option key={op.id} value={op.id}>
+              {op[sel]}
+              {separator}
+              {sel2 ? op[sel2] : ''}
+            </option>
+          ))}
+        </select>
+      </div>
     </div>
+
+
   )
 })
